8bd901f6911bebb7a629cbd319d07261f7b69be5
unknown <znancytian@gmail.com>
2021-12-21 13:36:40 -0500

    [15-likes-model-admin] add likes model and admin

---
+++ likes/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+
+class Like(models.Model):
+    # https://docs.djangoproject.com/en/3.1/ref/contrib/contenttypes/#generic-relations
+    object_id = models.PositiveIntegerField()
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    # user liked content_object at created_at
+    content_object = GenericForeignKey('content_type', 'object_id')
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # this unique_together builds index <user, content_type, object_id>
+        # which can help to query what objects a specific user liked.
+        # <content_type, object_id, user> cannot.
+        unique_together = (('user', 'content_type', 'object_id'),)
+        index_together = (('content_type', 'object_id', 'created_at'),)
+
+    def __str__(self):
+        return '{} - {} liked {} {}'.format(
+            self.created_at,
+            self.user,
+            self.content_type,
+            self.object_id,
+        )
