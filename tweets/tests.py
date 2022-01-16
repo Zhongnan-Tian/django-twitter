e49dea12f3b38a0a0e3e1a075bb9546638be812b
unknown <znancytian@gmail.com>
2022-01-16 16:29:41 -0500

    [24-photos-model-admin] add model and admin for tweet photos

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,6 +1,8 @@
 from testing.testcases import TestCase
 from datetime import timedelta
 from utils.time_helpers import utc_now
+from tweets.constants import TweetPhotoStatus
+from tweets.models import TweetPhoto
 
 
 class TweetTests(TestCase):
@@ -24,3 +26,11 @@ class TweetTests(TestCase):
         self.create_like(user2, self.tweet)
         self.assertEqual(self.tweet.like_set.count(), 2)
 
+    def test_create_photo(self):
+        photo = TweetPhoto.objects.create(
+            tweet=self.tweet,
+            user=self.user1,
+        )
+        self.assertEqual(photo.user, self.user1)
+        self.assertEqual(photo.status, TweetPhotoStatus.PENDING)
+        self.assertEqual(self.tweet.tweetphoto_set.count(), 1)
